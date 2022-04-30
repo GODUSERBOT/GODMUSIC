edc5478e1b2427de0752462a7cbe81c9d5d519b6
GOD INVISIBLE <104170029+GODUSERBOT@users.noreply.github.com>
2022-04-30 20:32:09 +0530

    Update start.py

--- Gods/utils/inline/start.py
+++ Gods/utils/inline/start.py
@@ -5,7 +5,7 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton
 
 from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
-from Rockz import app
+from Gods import app
 
 
 def start_pannel(_):
